0fc92cb69f14a6475c0a495afa863eca98b5bb2c
Jake Novelli <jakenovelli11@gmail.com>
2021-04-06 17:06:49 -0700

    added working response for google api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,4 @@
-var searchBtn = $(".searchButton");
+var searchBtn = $(".btn");
 // no location variable as we will prompt the user to share their location upon entering the webpage;
 // var googleAPI = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=milpitas&key=" + googleKey;
 
@@ -6,14 +6,33 @@ var searchBtn = $(".searchButton");
 
 var googleKey = 'AIzaSyDeiBrvVx6CsYF4J34xVFLvm1ObiiPKrdQ';
 
-var origin = $('#originCity').val();
-var oState = $('#originState').val();
-var dCity = $('#destCity').val();
-var dState = $('#destState').val();
+var origin = 'Vacaville'
+var oState = 'California'
+var dCity = 'New York City'
+var dState = 'New York'
 
-var googleAPI = `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin},${oState}&destinations=${dCity},${dState}&key=${googleKey}`
+var googleAPI = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin},${oState}&destinations=${dCity},${dState}&key=AIzaSyCBZc_OSBv_nae-DTj4IXZ1x7Zb00XzcRQ`
 
-fetch(googleAPI).then(res => res.json()).then(data => {
-  var info = (data.rows[0].elements[0])
-  console.log(info.distance.text)
+$.ajax({
+    url: googleAPI,
+    method: 'GET'
+}).then(function(response){
+    console.log(response)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch(googleAPI).then(res => res.json()).then(data => {
+//   var info = (data.rows[0].elements[0])
+//   console.log(info.distance.text)
+// })
